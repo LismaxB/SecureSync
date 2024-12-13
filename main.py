650ccfea454f2e7479d897ca8763bb66e79f7c3e
LismaxB <lisara.yatiwala.lis@gmail.com>
2024-12-14 00:02:34 +0530

    feat: add core device monitoring functionality and requirements

---
+++ main.py
@@ -0,0 +1,7 @@
+from usb_monitor import monitor_usb
+
+def main():
+    monitor_usb()
+
+if __name__ == "__main__":
+    main()
---
+++ usb_monitor.py
@@ -0,0 +1,39 @@
+import os
+import platform
+import psutil
+
+def detect_usb_drives():
+    usb_drives = []
+    current_os = platform.system()
+    if current_os == "Windows":
+        partitions = psutil.disk_partitions()
+        for partition in partitions:
+            if 'removable' in partition.opts:
+                usb_drives.append(partition.mountpoint)
+    elif current_os == "Linux":
+        usb_drives = [os.path.join("/media", d) for d in os.listdir("/media")]
+    elif current_os == "Darwin":
+        usb_drives = [os.path.join("/Volumes", d) for d in os.listdir("/Volumes")]
+    else:
+        print("Operating System not supported.")
+
+    return usb_drives
+
+
+def monitor_usb():
+    print("Starting USB monitoring...")
+    previously_detected = set(detect_usb_drives())
+
+    try:
+        while True:
+            current_drives = set(detect_usb_drives())
+            new_drives = current_drives - previously_detected
+            if new_drives:
+                for drive in new_drives:
+                    print(f"USB Drive Connected: {drive}")
+            previously_detected = current_drives
+    except KeyboardInterrupt:
+        print("Stopping USB monitoring...")
+
+if __name__ == "__main__":
+    monitor_usb()
